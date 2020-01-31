e5b7264dba9d70d795c24ab925b55048e336afbd
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-01-31 10:55:30 -0800

    Routing is working

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,5 +1,10 @@
 import React from "react";
 import CharacterList from "./CharacterList";
+import styled from 'styled-components';
+
+const iamge = styled.image `
+  margin-left: 35%;
+`
 
 export default function WelcomePage() {
   return (
@@ -7,13 +12,12 @@ export default function WelcomePage() {
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
 
-        {/* <img
+        <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
-        /> */}
+        />
       </header>
-      <CharacterList />
     </section>
   );
 }
