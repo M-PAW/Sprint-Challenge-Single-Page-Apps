bd2edd89af4f0b431a0d865e42e9c7ca51ff803d
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-01-31 10:09:13 -0800

    Partial Functionality, api working

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,45 @@
 import React, { useEffect, useState } from "react";
-
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
 export default function CharacterList() {
+
   // TODO: Add useState to track data from useEffect
+  const [ character, setCharacter ] = useState([]);
+  const [ query, setQuery ] = useState("");
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then ( response => {
+      console.log(response)
+      setCharacter(response.data.results);
+    })
+    .catch ( error => {
+      console.log('Nothing to Display!', error);
+    })
   }, []);
-
+// const handleChange = e => {
+//   setQuery(e.target.value);
+// }
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <SearchForm />
+      <div className=" grid-view">
+        {character.map(characters => {
+          return (
+        <CharacterCard
+          key={characters.id}
+          image={characters.image}
+          name={characters.name}
+          status={characters.status}
+          species={characters.species}
+          gender={characters.gender}
+          />
+          );
+        })}
+      </div>
     </section>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 
 export default function SearchForm() {
- 
   return (
-    <section className="search-form">
-     // Add a search form here
+    <section className="search-form" >
+      <form >
+        <input
+          // onChange={handleChange}
+          type="text"
+          name="character"
+          id="character"
+          placeHolder="Search"
+          />
+      </form>
     </section>
   );
 }
