31df1f927fec02843dc7ac13a697bba984d64f71
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-01-31 12:44:37 -0800

    Final Push, late

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header.js";
 import WelcomePage from './components/WelcomePage';
-import SearchForm from "./components/SearchForm.js";
+
 import CharacterCard from "./components/CharacterCard.js";
 import { Route } from 'react-router-dom';
 import { domainToASCII } from "url";
@@ -12,7 +12,6 @@ export default function App() {
   return (
     <main>
       <Header />
-      <SearchForm />
       <Route exact path="/"> <WelcomePage /> </Route>
       <Route path="/CharacterList"> <CharacterList /> </Route>
     </main>
